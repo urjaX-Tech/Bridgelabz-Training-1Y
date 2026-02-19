717eef2cb51c3f631276c4664ec11b7872912aa9
urjaX-Tech <chaurasiaurja@gmail.com>
2026-02-20 00:42:57 +0530

    Delete Basic-Programs directory

--- Basic-Programs/Primitive-Data-Types/level-1/KmToMiles.java
+++
@@ -1,9 +0,0 @@
-public class KmToMiles {
-    public static void main(String[] args) {
-
-        double kilometer = 10.8;
-        double miles = kilometer / 1.6;
-
-        System.out.println("The distance " + kilometer + " km in miles is " + miles);
-    }
-}
